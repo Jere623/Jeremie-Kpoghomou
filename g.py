fc40f45a4499445b2530ea1d6c8212fd985c2c21
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2025-08-18 11:25:16 +0200

    Update g.py

--- g.py
+++ g.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from pathlib import Path
-#import segno  
+import segno   # ✅ Correction : réactiver l'import
 import io
 
 # ------------------------------
