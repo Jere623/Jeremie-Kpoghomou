948f9f23638b7c692f97b9afab859f4fd038a3e8
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2025-08-19 20:56:57 +0200

    Update oser_ok.py

--- oser_ok.py
+++ oser_ok.py
@@ -160,9 +160,9 @@ with right:
             
             "1. Oser rêver": "## 1. Oser rêver.\n\n**Pourquoi rêver est indispensable ?**\n\nSans rêve, on avance sans boussole. On subit les choix des autres, on vit dans la routine, c'est l'horizon qui guide nos pas.\n\n Exemple concret: l'innovation = c'est le fruit du rêve ",
             
-            "2. Choisir avec courage": "## 2. Passer de l’intention à l’action.\n\n** Pourquoi le choix fait peur ?**\n\n La clé du choix : il faut oser franchir le pas.\n\n Exemple :",
+            "2. Choisir avec courage": "## 2. Passer de l’intention à l’action.\n\n**Pourquoi le choix fait peur ?**\n\nLa clé du choix : il faut oser franchir le pas.\n\n Exemple :",
             
-            "3. Construire pas à pas": "## 3.** Faire de son avenir une réalité**\n\n Construire, c’est accepter le temps et l’effort.\n\n Les ingrédients de la construction :\n\n La discipline\n\n L’apprentissage\n\n La persévérance",
+            "3. Construire pas à pas": "##**3. Faire de son avenir une réalité**\n\n Construire, c’est accepter le temps et l’effort.\n\n Les ingrédients de la construction :\n\n La discipline\n\n L’apprentissage\n\n La persévérance",
            
             "4. Mon parcours comme illustration": "## 4. Mon parcours comme illustration\n\n Licence en Économie-Gestion\n\n Triple Master : Data Science, Statistiques appliquées, Finances publiques\n\n Forces : analyse, rigueur, curiosité face aux chiffres\n\n Orientation stratégique vers un domaine porteur.",
             
