1b114c2c46781927eb6080a4a4acfe11e2a19ade
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2025-09-30 22:02:37 +0200

    Update Formation_IA.py

--- Formation_IA.py
+++ Formation_IA.py
@@ -329,10 +329,10 @@ with right:
                 st.markdown("""
                         🔹 **Conclusion**
                         * Le CV est à la fois un outil marketing personnel et un passeport professionnel. 
-                        * Un bon CV doit être :
-                        a. Clair et lisible (forme). 
-                        b. Orienté résultats (contenu). 
-                        c. Adapté à l’offre (personnalisation). 
+                        * Un bon CV doit être:
+                        - Clair et lisible (forme). 
+                        - Orienté résultats (contenu). 
+                        - Adapté à l’offre (personnalisation). 
                         * Il ne garantit pas un emploi, mais augmente fortement les chances d’obtenir un entretien.
                         
                         """)
