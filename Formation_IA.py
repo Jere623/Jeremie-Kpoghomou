a38b5efeb36b8adf30ffac7b070a69bd00a55e07
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2025-10-02 21:40:31 +0200

    Update Formation_IA.py

--- Formation_IA.py
+++ Formation_IA.py
@@ -344,9 +344,10 @@ Application -> https://chatgpt.com/c/68da38ea-ff14-8322-a059-5e3ebd9ef856
                         - 📌 Design et mise en page
                         * CV Zety -> https://builder.zety.com/resume
                         * CV fisio -> https://www.canva.com/design/DAFvoRxQ0fA/9pE0j5BXtIDSamVabhJy5Q/edit
-                        * Montage Canva -> https://www.canva.com/design/DAGswU6QT2w/NHYgZxhHCb5jC1lSy7Fo1w/edit
-                        * Freepik -> https://www.freepik.com/pikaso/ai-image-generator?sign-up=email 
+                        * CV Freepik -> https://www.freepik.com/pikaso/ai-image-generator?sign-up=email                                               
                         * LM ChatGPT -> https://chatgpt.com/c/68da38ea-ff14-8322-a059-5e3ebd9ef856
+                        * Montage Canva -> https://www.canva.com/design/DAGswU6QT2w/NHYgZxhHCb5jC1lSy7Fo1w/edit
+                        * Image Gemini -> https://gemini.google.com/app/2e9240fdeb7d91ad
                          
                         """)
 
